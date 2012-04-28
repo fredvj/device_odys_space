359eb10b43cb8b512f11eaa4b3deb4932fea634e
Fred vom Jupiter <fredvjupiter@googlemail.com>
2012-04-29 00:14:19 +0200

    New branch + extract script for the Odys 2.3.3 ROM

--- extract-files.sh
+++ extract-files.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PROPS=../../../vendor/odys/space/proprietary/
+PROPS=../../../vendor/odys/space/proprietary/233/
 
 mkdir -p $PROPS
 mkdir -p $PROPS/modules
@@ -22,7 +22,7 @@ for f in libra_ftm.ko libra.ko librasdioif.ko
 done
 
 echo "    -------------------- Hardware libraries"
-for f in copybit.msm7k.so gralloc.msm7k.so lights.msm7k.so sensors.7x27.so
+for f in copybit.msm7k.so gralloc.msm7k.so lights.msm7k.so sensors.default.so
 	do adb pull /system/lib/hw/$f $PROPS/hw
 done
 
@@ -36,7 +36,7 @@ adb pull /system/bin/hci_qcomm_init $PROPS
 adb pull /system/bin/hciattach $PROPS
 
 echo "    -------------------- Radio and associated libraries"
-for f in libcm.so libdsm.so libdss.so libdll.so libgsdi_exp.so libgstk_exp.so libmmgsdilib.so libnv.so liboem_rapi.so liboncrpc.so libqmi.so libqueue.so libril-qc-1.so libwms.so libwmsts.so libsnd.so libdiag.so libril-qcril-hook-oem.so libgsl.so libauth.so libpbmlib.so libdsutils.so libnetmgr.so
+for f in libcm.so libdsm.so libdss.so libdl.so libgsdi_exp.so libgstk_exp.so libmmgsdilib.so libnv.so liboem_rapi.so liboncrpc.so libqmi.so libqueue.so libril-qc-1.so libwms.so libwmsts.so libsnd.so libdiag.so libril-qcril-hook-oem.so libgsl.so libauth.so libpbmlib.so libdsutils.so libnetmgr.so
 	do adb pull /system/lib/$f $PROPS
 done
 
@@ -58,7 +58,7 @@ echo "    -------------------- GPS library"
 # Others are saying we need only libloc-rpc.so libloc.so & libcommondefs.so
 # Pulling the rest anyway
 
-for f in libgps.so libloc-rpc.so libloc.so libloc_api.so libloc_ext.so libcommondefs.so 
+for f in libgps.so libloc-rpc.so libloc_ext.so libloc_api-rpc-qc.so libcommondefs.so 
 	do adb pull /system/lib/$f $PROPS
 done
 
@@ -72,13 +72,13 @@ echo "    -------------------- WLAN Firmware"
 adb pull /persist/qcom_wlan_nv.bin $PROPS/prebuilt/
 adb pull /persist/qcom/softap/qcom_cfg_default.ini $PROPS/prebuilt/
 adb pull /persist/qcom/softap/hostapd_default.conf $PROPS/prebuilt/
-adb pull /data/hostapd/qcom_cfg.ini $PROPS/prebuilt/
 adb pull /data/hostapd/hostapd.conf $PROPS/prebuilt/
 touch $PROPS/prebuilt/hostapd.deny
 touch $PROPS/prebuilt/hostapd.accept
 adb pull /etc/firmware/wlan/cfg.dat $PROPS/prebuilt/
 adb pull /etc/firmware/wlan/qcom_wapi_fw.bin $PROPS/prebuilt/
 adb pull /etc/firmware/wlan/qcom_fw.bin $PROPS/prebuilt/
+adb pull /etc/firmware/wlan/qcom_cfg.ini
 chmod a+rx $PROPS/prebuilt/*
 
 
@@ -90,7 +90,7 @@ chmod a+rx $PROPS/prebuilt/*
 # init.qcom.sdio.sh
 # init.qcom.wifi.sh
 
-echo "    -------------------- Android 2.2.2 init scripts"
+echo "    -------------------- Android 2.3.3 init scripts"
 for f in init.qcom.bt.sh init.qcom.coex.sh init.qcom.fm.sh init.qcom.post_boot.sh init.qcom.sdio.sh init.qcom.wifi.sh 01_qcomm_omx.cfg loc_parameter.ini pvplayer.cfg
 	do adb pull /system/etc/$f $PROPS/etc/
 done
@@ -103,20 +103,18 @@ adb pull /init.qcom.rc .
 
 adb pull /init.rc init.space.rc
 
-echo "    -------------------- Other prebuilt binaries from Anroid 2.2.2 image"
+echo "    -------------------- Other prebuilt binaries from Anroid 2.3.3 image"
 
 adb pull /system/bin/battery_charging $PROPS/prebuilt/
 adb pull /system/bin/qmuxd $PROPS/prebuilt/
 adb pull /system/bin/btwlancoex $PROPS/prebuilt/
 adb pull /system/bin/wifiwritemac $PROPS/prebuilt/
 adb pull /system/bin/port-bridge $PROPS/prebuilt/
-adb pull /system/bin/CKPD-daemon $PROPS/prebuilt/
 adb pull /system/bin/hostapd $PROPS/prebuilt/
 adb pull /system/bin/hostapd_cli $PROPS/prebuilt/
 adb pull /system/bin/ndc $PROPS/prebuilt/
 adb pull /system/bin/netd $PROPS/prebuilt/
 adb pull /system/bin/fm_qsoc_patches $PROPS/prebuilt/
-adb pull /system/bin/wiperiface $PROPS/prebuilt/
 adb pull /system/bin/wpa_supplicant $PROPS/prebuilt/
 adb pull /system/bin/rmt_storage $PROPS/prebuilt/
 adb pull /system/bin/netmgrd $PROPS/prebuilt/
