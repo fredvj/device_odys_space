cb48e8d4350f87407651bcb7f2011b15dc821b20
fredvj <fredvjupiter@googlemail.com>
2012-12-09 17:43:24 +0100

    First round of ICS cleanups

--- device_space.mk
+++ device_space.mk
@@ -7,29 +7,37 @@ $(call inherit-product-if-exists, vendor/odys/space/space-vendor.mk)
 
 PRODUCT_PACKAGES += \
 	make_ext4fs \
-	gralloc.msm7x27 \
-	hwcomposer.msm7x27 \
-	audio.a2dp.default \
-	audio.primary.space \
-	audio_policy.space \
-	hwcomposer.default \
 	DSPManager \
-	MusicFX
+	MusicFX \
+	Torch \
+	setup_fs
 
-# This is the list of libraries to include in the build
-# Sensors are ours
-# Lights coming from hardware/msm7k/liblights
-
+# Graphics
 PRODUCT_PACKAGES += \
-	libOmxCore \
-	libOmxVidEnc \
-	libmm-omxcore \
-	libstagefrighthw \
+	gralloc.msm7x27 \
+	copybit.msm7x27 \
+        hwcomposer.msm7x27 \
 	libgenlock \
-	liboverlay \
+	libQcomUI \
 	libtilerenderer \
-	libQcomUI
+	libmemalloc \
+	liboverlay
 
+# Audio
+PRODUCT_PACKAGES += \
+        audio.a2dp.default \
+        audio.primary.msm7x27 \
+        audio_policy.msm7x27 \
+	libaudioutils
+
+# QCOM OMX
+PRODUCT_PACKAGES += \
+	libstagefrighthw \
+	libOmxCore \
+	libOmxVdec \
+	libOmxVenc \
+	libmm-omxcore \
+	libdivxdrmdecrypt
 
 # A kernel seems like a good start
 
@@ -52,56 +60,31 @@ PRODUCT_COPY_FILES += \
 	device/odys/space/prebuilt/7k_ffa_keypad.kcm.bin:system/usr/keychars/7k_ffa_keypad.kcm.bin \
 	device/odys/space/prebuilt/ft5x0x_ts.idc:system/usr/idc/ft5x0x_ts.idc
 
-# Install device features
+# Permissions
 
 PRODUCT_COPY_FILES += \
-	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml \
-	frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-	frameworks/base/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
-	frameworks/base/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
-	frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-	frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-	frameworks/base/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
-	frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-	frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-	frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
-	frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
-	frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
-	frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml
-
-# Qualcomm permission files & framework extensions ?
-
-PRODUCT_COPY_FILE += \
-	device/odys/space/config/com.qualcomm.location.vzw_library.xml:system/etc/permissions/com.qualcomm.location.vzw_library.xml \
-	device/odys/space/config/qcnvitems.xml:system/etc/permissions/qcnvitems.xml \
-	device/odys/space/config/qcrilhook.xml:system/etc/permissions/qcrilhook.xml \
-	device/odys/space/config/com.qualcomm.location.vzw_library.jar:system/framework/com.qualcomm.location.vzw_library.jar \
-	device/odys/space/config/qcnvitems.jar:system/framework/qcnvitems.jar \
-	device/odys/space/config/qcrilhook.jar:system/framework/qcrilhook.jar
-
+        frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+        frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+        frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+        frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+        frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+        frameworks/base/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+        frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+        frameworks/base/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
+        frameworks/base/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+        frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+        frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+        frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+        frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+        frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+        frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+        frameworks/base/data/etc/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
+        packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
 
 # Boot logo
 
 PRODUCT_COPY_FILES += \
 	device/odys/space/prebuilt/initlogo.rle:root/initlogo.rle
-
-# APNS / SPN
-#
-# It looks like Cyanogen has more power - apns-conf.xml gets overwritten by
-# development/data/etc/apns-conf.xml
-#
-# Start here and edit:
-# vendor/cyanogen/prebuilt/etc/apns-conf.xml
-# copy to:
-# development/data/etc/apns-conf.xml
-#
-# Do not edit here (would result in duplicate entries):
-# frameworks/base/core/res/res/xml/apns.xml
-
-# PRODUCT_COPY_FILES += \
-#	device/odys/space/prebuilt/apns-conf.xml:system/etc/apns-conf.xml \
-#	device/odys/space/prebuilt/spn-conf.xml:system/etc/spn-conf.xml \
-#	device/odys/space/prebuilt/voicemail-conf.xml:system/etc/voicemail-conf.xml
 
 # Startup scripts
 # If would like to name it init.space.rc, we would have to change the kernel parameter
@@ -143,32 +126,18 @@ PRODUCT_COPY_FILES += \
 	vendor/odys/space/proprietary/233/modules/libra.ko:system/lib/modules/libra.ko \
 	vendor/odys/space/proprietary/233/modules/librasdioif.ko:system/lib/modules/librasdioif.ko
 
-# Hardware libraries
-
-# gralloc are copied from old ROM (the new one is using too much memory?)
-
-# PRODUCT_COPY_FILES += \
-# 	vendor/odys/space/proprietary/233/hw/gps.default.so:system/lib/hw/gps.default.so \
-# 	vendor/odys/space/proprietary/233/hw/lights.msm7k.so:system/lib/hw/lights.msm7k.so \
-# 	vendor/odys/space/proprietary/233/hw/sensors.default.so:system/lib/hw/sensors.default.so \
-# 	vendor/odys/space/proprietary/233/hw/copybit.msm7k.so:system/lib/hw/copybit.msm7k.so \
-# 	vendor/odys/space/proprietary/233/hw/gralloc.msm7k.so:system/lib/hw/gralloc.msm7k.so
-
 # Graphic hardware
 
 PRODUCT_COPY_FILES += \
-	device/odys/space/prebuilt/adreno200/system/lib/egl/egl.cfg:system/lib/egl/egl.cfg \
-	device/odys/space/prebuilt/adreno200/system/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
-	device/odys/space/prebuilt/adreno200/system/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
-	device/odys/space/prebuilt/adreno200/system/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
-	device/odys/space/prebuilt/adreno200/system/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
-	device/odys/space/prebuilt/adreno200/system/lib/egl/libGLES_android.so:system/lib/egl/libGLES_android.so \
-	device/odys/space/prebuilt/adreno200/system/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
-	device/odys/space/prebuilt/adreno200/system/lib/libC2D2.so:system/lib/libC2D2.so \
-	device/odys/space/prebuilt/adreno200/system/lib/libgsl.so:system/lib/libgsl.so \
-	device/odys/space/prebuilt/adreno200/system/lib/libOpenVG.so:system/lib/libOpenVG.so \
-	device/odys/space/prebuilt/adreno200/system/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so
-
+	device/odys/space/prebuilt/adreno200/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
+	device/odys/space/prebuilt/adreno200/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
+	device/odys/space/prebuilt/adreno200/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
+	device/odys/space/prebuilt/adreno200/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
+	device/odys/space/prebuilt/adreno200/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
+	device/odys/space/prebuilt/adreno200/lib/libC2D2.so:system/lib/libC2D2.so \
+	device/odys/space/prebuilt/adreno200/lib/libgsl.so:system/lib/libgsl.so \
+	device/odys/space/prebuilt/adreno200/lib/libOpenVG.so:system/lib/libOpenVG.so \
+	device/odys/space/prebuilt/adreno200/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so
 
 # Bluetooth helpers
 
@@ -203,11 +172,6 @@ PRODUCT_COPY_FILES += \
 	vendor/odys/space/proprietary/233/libidl.so:system/lib/libidl.so \
 	vendor/odys/space/proprietary/233/libril-qc-qmi-1.so:system/lib/libril-qc-qmi-1.so
 
-
-# 	vendor/odys/space/proprietary/233/libdsutils.so:system/lib/libdsutils.so \
-# 	vendor/odys/space/proprietary/233/libnetmgr.so:system/lib/libnetmgr.so
-
-
 # Camera control and encoding libraries
 
 # Build environment (obj)
@@ -229,27 +193,19 @@ PRODUCT_COPY_FILES += \
 	device/samsung/gio/prebuilt/camera.gio.so:system/lib/hw/camera.gio.so \
 	device/samsung/gio/prebuilt/camera.gio.so:system/lib/hw/camera.space.so \
 	device/samsung/gio/prebuilt/libcamera.so:system/lib/libcamera.so \
-	device/samsung/gio/prebuilt/libcamera_client.so:system/lib/libcamera_client.so \
-	device/samsung/gio/prebuilt/libcameraservice.so:system/lib/libcameraservice.so \
 	device/samsung/gio/prebuilt/libmm-adspsvc.so:system/lib/libmm-adspsvc.so \
 	device/samsung/gio/prebuilt/libmmgsdilib.so:system/lib/libmmgsdilib.so \
 	device/samsung/gio/prebuilt/libmmipl.so:system/lib/libmmipl.so \
 	device/samsung/gio/prebuilt/libmmjpeg.so:system/lib/libmmjpeg.so \
-	device/samsung/gio/prebuilt/libmm-omxcore.so:system/lib/libmm-omxcore.so \
 	device/samsung/gio/prebuilt/liboemcamera.so:system/lib/liboemcamera.so
 
 # Firmware
 
 PRODUCT_COPY_FILES += \
-        device/odys/space/prebuilt/adreno200/system/etc/firmware/a225p5_pm4.fw:system/etc/firmware/a225p5_pm4.fw \
-        device/odys/space/prebuilt/adreno200/system/etc/firmware/a225_pfp.fw:system/etc/firmware/a225_pfp.fw \
-        device/odys/space/prebuilt/adreno200/system/etc/firmware/a225_pm4.fw:system/etc/firmware/a225_pm4.fw \
-        device/odys/space/prebuilt/adreno200/system/etc/firmware/a300_pfp.fw:system/etc/firmware/a300_pfp.fw \
-        device/odys/space/prebuilt/adreno200/system/etc/firmware/a300_pm4.fw:system/etc/firmware/a300_pm4.fw \
-        device/odys/space/prebuilt/adreno200/system/etc/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
-        device/odys/space/prebuilt/adreno200/system/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
-	device/odys/space/prebuilt/adreno200/system/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
-	device/odys/space/prebuilt/adreno200/system/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
+        device/odys/space/prebuilt/adreno200/etc/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
+        device/odys/space/prebuilt/adreno200/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
+	device/odys/space/prebuilt/adreno200/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
+	device/odys/space/prebuilt/adreno200/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
 	vendor/odys/space/proprietary/233/prebuilt/cfg.dat:system/etc/firmware/wlan/cfg.dat \
 	vendor/odys/space/proprietary/233/prebuilt/qcom_wapi_fw.bin:system/etc/firmware/wlan/qcom_wapi_fw.bin \
 	vendor/odys/space/proprietary/233/prebuilt/qcom_fw.bin:system/etc/firmware/wlan/qcom_fw.bin \
@@ -294,8 +250,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_LOCALES += en
 PRODUCT_AAPT_CONFIG := normal ldpi mdpi
 PRODUCT_AAPT_PREF_CONFIG := mdpi
-# $(call inherit-product, build/target/product/full_base.mk)
-# $(call inherit-product, build/target/product/languages_small.mk)
 
 # HardwareRenderer properties
 # dirty_regions: "false" to disable partial invalidates, override if enabletr=true
@@ -308,9 +262,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     pm.sleep_mode=1
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := odys_space
+PRODUCT_NAME := full_space
 PRODUCT_DEVICE := space
-PRODUCT_MODEL := Odys Space
+PRODUCT_MODEL := space
 PRODUCT_BRAND := ACTION
 PRODUCT_MANUFACTURER := ACTION
 
